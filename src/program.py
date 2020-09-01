d946ad0fda8e945e9c336f78fb9411d40159f1b9
RobertFent <robert.fent@live.com>
2020-09-01 20:43:55 +0200

    fixed some issues for calculating exiting condition and calculation of basecoins to sell

--- src/program.py
+++ src/program.py
@@ -29,9 +29,6 @@ DEFAULT_SYMBOL = BASE_COIN + QUOTE_COIN
 DEFAULT_STATE = os.getenv('DEFAULT_STATE')
 DEFAULT_DECIMAL_PLACES = os.getenv('DEFAULT_DECIMAL_PLACES')
 
-# needed for later parsing
-base_coin_len = len(BASE_COIN)
-
 DEFAULT_PERCENTAGE = float(os.getenv('DEFAULT_PERCENTAGE'))
 
 # Thresholds for buy and sell
@@ -54,28 +51,38 @@ STOP_LOSS_THRESHOLD = float(os.getenv('STOP_LOSS_THRESHOLD'))
 
 profit_arr = []
 
-# todo decide where to parse float to str
 
+# todo decide where to parse float to str
 def init_classes(testing, init_state, symbol):
     calc = Calculator()
     logging.info('Init bot with symbol: %s' % (symbol))
     if testing:
-        bot = TradingBot(API_KEY_TEST, SECRET_KEY_TEST,
-                         BASE_ENDPOINT_TEST, init_state, symbol)
+        bot = TradingBot(API_KEY_TEST, SECRET_KEY_TEST, BASE_ENDPOINT_TEST,
+                         init_state, symbol)
     else:
-        bot = TradingBot(API_KEY, SECRET_KEY,
-                         BASE_ENDPOINT, init_state, symbol)
+        bot = TradingBot(API_KEY, SECRET_KEY, BASE_ENDPOINT, init_state,
+                         symbol)
     return calc, bot
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser('My Trading Bot')
-    parser.add_argument('symbol', type=str, default=DEFAULT_SYMBOL,
-                        help='Symbol the bot shall trade', nargs='?')
-    parser.add_argument('percentage', type=float, default=DEFAULT_PERCENTAGE,
-                        help='Amount of current balance the bot shall trade', nargs='?')
-    parser.add_argument('decimal_places', type=int, default=DEFAULT_DECIMAL_PLACES, 
-                        help='Number of decimal places behind coin for trading', nargs='?')
+    parser.add_argument('symbol',
+                        type=str,
+                        default=DEFAULT_SYMBOL,
+                        help='Symbol the bot shall trade',
+                        nargs='?')
+    parser.add_argument('percentage',
+                        type=float,
+                        default=DEFAULT_PERCENTAGE,
+                        help='Amount of current balance the bot shall trade',
+                        nargs='?')
+    parser.add_argument(
+        'decimal_places',
+        type=int,
+        default=DEFAULT_DECIMAL_PLACES,
+        help='Number of decimal places behind coin for trading',
+        nargs='?')
     args = parser.parse_args()
     symbol = args.symbol
     percentage = round(args.percentage, 2)
@@ -84,123 +91,135 @@ def parse_arguments():
 
 
 def get_readable_timestamp():
-    return '%sh:%smin:%ssec' % (time.localtime().tm_hour, time.localtime().tm_min, time.localtime().tm_sec)
+    return '%sh:%smin:%ssec' % (time.localtime().tm_hour,
+                                time.localtime().tm_min,
+                                time.localtime().tm_sec)
 
 
-def bot_loop(calc, bot, symbol, starting_balance, percentage_balance, decimal_places):
+def bot_loop(calc, bot, symbol, starting_balance, percentage_balance,
+             decimal_places):
     default_price = float(bot.get_symbol_price())
     i = 0
     while True:
         current_price = float(bot.get_symbol_price())
-        total_price_change = calc.get_total_change(
-            current_price, default_price)
+        total_price_change = calc.get_total_change(current_price,
+                                                   default_price)
         percentage_price_change = round(
             calc.get_percentage_change(current_price, default_price), 3)
 
         if i % 5 == 0:
             print('\n\nTime: %s' % (get_readable_timestamp()))
+            print('Default price: %f' % (default_price))
+            print('Current price: %f' % (current_price))
             print('Total price change: %f' % (total_price_change))
             print('Percentage price change: %2.2f' % (percentage_price_change))
 
         if should_exit(bot, symbol, starting_balance):
             raise Exception('Time to go now')
 
-        if do_next_action(percentage_price_change, bot, calc, symbol, current_price, percentage_balance, decimal_places):
+        if do_next_action(percentage_price_change, bot, calc, symbol,
+                          current_price, percentage_balance, decimal_places):
+            print('Bought or sold')
+            print('Current default price %f' % (default_price))
             default_price = current_price
+            print('Setting new default price to %f' % (default_price))
         time.sleep(30)
         i += 1
 
 
 # returns true if current balance is twice as great or half starting balance
-# todo bug her
-'''
-Time: 14h:59min:48sec
-Total price change: 0.000018
-Percentage price change: 1.91
-
-
-Time: 15h:2min:22sec
-Total price change: 0.000000
-Percentage price change: 0.00
-Time to go now
-Traceback (most recent call last):
-  File "src/program.py", line 203, in <module>
-    except Exception as inst:
-  File "src/program.py", line 199, in main
-    try:
-  File "src/program.py", line 107, in bot_loop
-    raise Exception('Time to go now')
-Exception: Time to go now
-'''
-# todo also magic numbers
+# todo magic numbers
 def should_exit(bot, symbol, starting_balance):
-    current_balance = float(bot.get_coin_amount(symbol[base_coin_len:]))
-    if current_balance < float(starting_balance)/3 or current_balance > float(starting_balance)*2:
+    current_balance = float(bot.get_coin_amount(symbol[len(BASE_COIN):]))
+    if current_balance < float(starting_balance) / 3 or current_balance > float(
+            starting_balance) * 2:
         print('Exit!')
-        print('Current balance: %f' %(current_balance))
-        print('Starting balance: %f' %(starting_balance))
+        print('Current balance: %f' % (current_balance))
+        print('Starting balance: %f' % (starting_balance))
         return True
     return False
 
+
 # todo? round needed?
-def calc_trading_coins(calc, current_balance, percentage_balance, current_price, decimal_places):
-    base_coins = calc.get_amount_by_percentage(percentage_balance, current_balance)
+def calc_trading_coins(calc, current_balance, percentage_balance,
+                       current_price, decimal_places):
+    base_coins = calc.get_amount_by_percentage(percentage_balance,
+                                               current_balance)
     trading_coins = round(base_coins / current_price, decimal_places)
     if (decimal_places < 1): return int(trading_coins)
     return trading_coins
 
 
-
-def log_trade(state, bot, symbol, percentage_price_change, coins, current_avg_price):
-    current_balance_base = bot.get_coin_amount(symbol[:base_coin_len])
-    current_balance_quote = bot.get_coin_amount(symbol[base_coin_len:])
-    logging.info('%s\n%s: %2.2f percent change' % (get_readable_timestamp(), state, percentage_price_change))
+def log_trade(state, bot, symbol, percentage_price_change, coins,
+              current_avg_price):
+    current_balance_base = bot.get_coin_amount(symbol[:len(BASE_COIN)])
+    current_balance_quote = bot.get_coin_amount(symbol[len(BASE_COIN):])
+    logging.info('%s\n%s: %2.2f percent change' %
+                 (get_readable_timestamp(), state, percentage_price_change))
     verb = 'Bought' if state == 'BUY' else 'Sold'
     logging.info('%s %fcoins for %s' % (verb, coins, current_avg_price))
-    logging.info('NEW BALANCE\n%s: %s\n%s: %s' % (symbol[:base_coin_len], current_balance_base, symbol[base_coin_len:], current_balance_quote))
+    logging.info('NEW BALANCE\n%s: %s\n%s: %s' %
+                 (symbol[:len(BASE_COIN)], current_balance_base,
+                  symbol[len(BASE_COIN):], current_balance_quote))
 
 
 # returns true if bot did sell or buy
-def do_next_action(percentage_price_change, bot, calc, symbol, current_price, percentage_balance, decimal_places):
+def do_next_action(percentage_price_change, bot, calc, symbol, current_price,
+                   percentage_balance, decimal_places):
     if (bot.get_state() == 'BUY'):
         if percentage_price_change <= DIP_THRESHOLD or percentage_price_change >= UPWARD_TREND_THRESHOLD:
-            current_balance_quote = bot.get_coin_amount(symbol[base_coin_len:])
+            current_balance_quote = bot.get_coin_amount(
+                symbol[len(BASE_COIN):])
             print('I buy now')
-            coins = calc_trading_coins(calc, current_balance_quote, percentage_balance, current_price, decimal_places)
+            # buy coins depending on wanted percentage of total balance
+            coins = calc_trading_coins(calc, current_balance_quote,
+                                       percentage_balance, current_price,
+                                       decimal_places)
             print('%f coins for %f' % (coins, current_price))
             bot.send_order_total('MARKET', coins)
-            log_trade('BUY', bot, symbol, percentage_price_change, coins, current_price)
+            log_trade('BUY', bot, symbol, percentage_price_change, coins,
+                      current_price)
             bot.change_state()
             logging.info('STATE: SELL')
             return True
     else:
         if percentage_price_change <= STOP_LOSS_THRESHOLD or percentage_price_change >= PROFIT_THRESHOLD:
-            current_balance_quote = bot.get_coin_amount(symbol[base_coin_len:])
+            current_balance_quote = bot.get_coin_amount(
+                symbol[len(BASE_COIN):])
             print('I sell now')
             print('The profit would be %f' % (percentage_price_change))
             profit_arr.append(percentage_price_change)
-            coins = calc_trading_coins(calc, current_balance_quote, percentage_balance, current_price, decimal_places)
+            # sell all base coins (ADA, FET, NEO etc.)
+            coins = round(float(bot.get_coin_amount(symbol[:len(BASE_COIN)])), decimal_places)
+            #coins = calc_trading_coins(calc, current_balance_quote,
+            #                           percentage_balance, current_price,
+            #                           decimal_places)
             bot.send_order_total('MARKET', coins)
-            log_trade('SELL', bot, symbol, percentage_price_change, coins, current_price)
+            log_trade('SELL', bot, symbol, percentage_price_change, coins,
+                      current_price)
             bot.change_state()
             logging.info('STATE: BUY')
             profit_arr.append(percentage_price_change)
             return True
     return False
 
+
 def parse_init_state(symbol):
     text = ''
     with open('logs/trade_history_%s.log' % (symbol), 'r') as file:
         text = file.read()
     states = re.findall('STATE: (BUY|SELL)', text)
-    # return state before last state
-    if (len(states) > 1):
-        return states[len(states) - 2]
+    # return last state
+    if (len(states) > 0):
+        return states[len(states) - 1]
     return DEFAULT_STATE
 
+
 def init_logger(symbol):
-    logging.basicConfig(filename='./logs/trade_history_%s.log' % (symbol), level=logging.INFO)
+    logging.basicConfig(filename='./logs/trade_history_%s.log' % (symbol),
+                        level=logging.INFO)
     logging.info(get_readable_timestamp())
+
 
 # todo split log in loop in methods
 def main():
@@ -209,14 +228,21 @@ def main():
     init_state = parse_init_state(symbol)
     calc, bot = init_classes(False, init_state, symbol)
 
+    # import balance; always quote coin (bnb, btc)
+    starting_balance = float(bot.get_coin_amount(symbol[len(BASE_COIN):]))
+    starting_balance_base = float(bot.get_coin_amount(symbol[:len(BASE_COIN)]))
     logging.info(bot.get_account_information()['balances'])
-    logging.info('Percentage of basecoin balance the bot will trade with: %2.2f' % (percentage))
+    logging.info(
+        'Percentage of basecoin balance the bot will trade with: %2.2f' %
+        (percentage))
 
     print(symbol)
-    print('Percentage of balance the bot will trade with: %2.2f' % (percentage))
+    print('Init state: %s' % (init_state))
+    print('Starting balance quote (%s): %f' % (symbol[len(BASE_COIN):], starting_balance))
+    print('Starting balance base (%s): %f' % (symbol[:len(BASE_COIN)], starting_balance_base))
+    print('Percentage of balance the bot will trade with: %2.2f' %
+          (percentage))
     print('Decimal places of base coin: %d' % (decimal_places))
-    # import balance always quote coin (bnb, btc)
-    starting_balance = float(bot.get_coin_amount(symbol[base_coin_len:]))
 
     bot_loop(calc, bot, symbol, starting_balance, percentage, decimal_places)
 
@@ -236,7 +262,6 @@ except Exception as inst:
 
 # print(bot.get_order_book(SYMBOL))
 # print(bot.get_daily_stats(SYMBOL))
-
 """
 Time: 19h:3min:50sec
 Total price change: -0.000001
@@ -257,7 +282,6 @@ I sell now
 The profit would be 0.061000 # todo
 Starting to log
 """
-
 '''
 # todo sell calc?
 ❯ python3 src/program.py 'FETBNB'
